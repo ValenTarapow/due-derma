f27da1508963f4933b22c071850224f020810291
Valentina Tarapow <valentina.tarapow@brosoft.com.ar>
2025-09-13 01:36:22 -0300

    added colors util

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    black: "#0c0c0c",
+    white: "#ffffff",
+    lightGray: "#f9f8f7",
+    beige: "#f0efec",
+    lightBlue: "#e0e3e7",
+    blueGray: "#9299a1",
+};
+
+export default colors;
